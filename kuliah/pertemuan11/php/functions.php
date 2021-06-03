cf68b013c954a03bf4160bc61b1aa40de1e6d563
rafiramadhannn <rafiramadhan711@gmail.com>
2021-06-03 16:04:03 +0700

    menyelesaikan tugas pertemuan kuliah pw

---
+++ kuliah/pertemuan11/php/functions.php
@@ -0,0 +1,91 @@
+<?php
+function koneksi()
+{
+    return mysqli_connect("localhost", "root", "", "pw2021_203040051");
+}
+function query($query)
+{
+    $conn = koneksi();
+    $result = mysqli_query($conn, $query);
+
+    // jika hasilnya hanya satu data
+    if (mysqli_num_rows($result) == 1) {
+        return mysqli_fetch_assoc($result);
+    }
+
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data)
+{
+    // ambil data dari tiap elemen dalam form
+    $conn = koneksi();
+    $nama = htmlspecialchars($data['nama']);
+    $nrp = htmlspecialchars($data['nrp']);
+    $email = htmlspecialchars($data['email']);
+    $jurusan = htmlspecialchars($data['jurusan']);
+    $img = htmlspecialchars($data['img']);
+
+    // query insert data
+    $query = "INSERT INTO mahasiswa
+                    VALUES
+                    ('', '$nama', '$nrp', '$email', '$jurusan', '$img')";
+    
+    mysqli_query($conn, $query) or die(mysqli_error($conn));
+    echo mysqli_error($conn);
+
+    return mysqli_affected_rows($conn);
+}
+
+function hapus($id) {
+    $conn = koneksi();
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id") or die(mysqli_error($conn));
+    return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+    // ambil data dari tiap elemen dalam form
+    $conn = koneksi();
+    $id = $data['id'];
+    $nama = htmlspecialchars($data['nama']);
+    $nrp = htmlspecialchars($data['nrp']);
+    $email = htmlspecialchars($data['email']);
+    $jurusan = htmlspecialchars($data['jurusan']);
+    $img = htmlspecialchars($data['img']);
+
+    // query update data
+    $query ="UPDATE mahasiswa SET
+                nama = '$nama',
+                nrp = '$nrp',
+                email = '$email',
+                jurusan = '$jurusan',
+                img = '$img'
+             WHERE id = $id";
+                
+    mysqli_query($conn, $query) or die(mysqli_error($conn));
+    echo mysqli_error($conn);
+
+    return mysqli_affected_rows($conn);
+}
+
+function cari($keyword) 
+{
+    $conn =  koneksi();
+
+    $query = "SELECT * FROM mahasiswa 
+                WHERE nama LIKE '%$keyword%'";
+    
+    $result =  mysqli_query($conn, $query);
+
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+
+}
